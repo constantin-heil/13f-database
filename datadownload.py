8cbc82e14e17e8f1b247a2deedbe61546b037c42
constantin heil <constantin.heil@gmail.com>
2023-11-19 18:39:40 +0100

    added specifications for data tables

--- datadownload.py
+++ datadownload.py
@@ -7,6 +7,13 @@ import pandas as pd
 import zipfile
 import pymysql
 from sqlalchemy import create_engine, text
+import json
+
+def import_json(fn: str) -> dict:
+    with open(fn, 'r') as fh:
+        loadedjson = json.load(fh)
+
+    return loadedjson
 
 username = 'remoteuser'
 password = 'password'
@@ -20,9 +27,14 @@ with engine.connect() as c:
 
 engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{database}")
 
+wd = Path(__file__).resolve().parent
+
+coltypes = import_json(wd / "dataspecs/columntypes.json")
+datecols = import_json(wd / "dataspecs/dateix.json")
+
 TARGETSITE = "https://www.sec.gov/dera/data/form-13f"
 BASEURL = "https://www.sec.gov"
-TMPDATAPATH = Path(__file__).resolve().parent / "datapath"
+TMPDATAPATH = wd / "datapath"
 
 target_html = requests.get(TARGETSITE).content
 target_parsed = bs4.BeautifulSoup(target_html, features = "html.parser")
@@ -48,8 +60,14 @@ for dp in datapaths:
 
     datafiles = TMPDATAPATH.rglob("*.tsv")
     filenames_dic = {str(p.stem): str(p.resolve()) for p in datafiles}
+
     for file in filenames_dic:
-        df = pd.read_csv(filenames_dic[file], sep = "\t")
+        df = pd.read_csv(filenames_dic[file], 
+                         sep = "\t", 
+                         parse_dates = datecols[file], 
+                         date_format = "%d-%b-%Y",
+                         dtype = coltypes[file])
+
         df.to_sql(file, con = engine, index = False, if_exists = "append")
 
         if file == "SUBMISSION":
